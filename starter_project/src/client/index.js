5bcbf94d70a803219e94da5f51df8e8cee73673b
ManarSholi <manar.ghassan.sholi@gmail.com>
2025-01-26 13:54:28 +0200

    - install webpack, webpack-cli, webpack-dev-server. - add mini-css plugin, optimize-css plugin, clean-webpack-plugin. - add node-sass, sass-loader, style-loader, css-loader. - add babel/core, preset-env, loader. - convert style files from css to scss. - import scss files in index.js.

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -3,7 +3,14 @@ import { handleSubmit } from './js/formHandler'
 
 
 
-// alert("I EXIST")
+alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+
+export {handleSubmit};
